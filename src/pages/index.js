32d8d80495ba16b0a819c940d31144f028f204b3
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2023-09-06 21:11:20 +0530

    added: profile page and update profile feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import CreateNotePage from "./CreateNotePage/CreateNotePage";
 import LandingPage from "./LandingPage/LandingPage";
 import LoginPage from "./LoginPage/LoginPage";
 import MyNotes from "./MyNotesPage/MyNotes";
+import ProfilePage from "./ProfilePage/ProfilePage";
 import RegisterPage from "./RegisterPage/RegisterPage";
 import SingleNotePage from "./SingleNotePage/SingleNotePage";
 
@@ -9,6 +10,7 @@ export {
   CreateNotePage,
   LandingPage,
   MyNotes,
+  ProfilePage,
   LoginPage,
   RegisterPage,
   SingleNotePage,
